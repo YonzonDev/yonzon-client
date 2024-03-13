342a79bef4a590997f276a65cb365301e5b00854
jbrygutierrez <jerome.gutierrez@lexmeet.com>
2024-03-14 00:27:28 +0800

    fix: axios url

--- src/redux/constants/url.js
+++ src/redux/constants/url.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://yonzon-api.onrender.com";
+export const baseURL = "https://yonzon-server.onrender.com";
 // export const baseURL = "http://127.0.0.1:8080";
 
 export const headers = {
